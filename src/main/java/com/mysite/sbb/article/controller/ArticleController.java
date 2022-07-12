345cf4afe318478f214576fb575fae6da3ae0942
leeyeonji28 <yeon102800@gmail.com>
2022-07-12 13:46:46 +0900

    게시글 제목, 내용 검색

--- src/main/java/com/mysite/sbb/article/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -26,7 +26,29 @@ public class ArticleController {
 
     @RequestMapping("/list")
     @ResponseBody
-    public List<Article> showArticleList() {
+    public List<Article> showArticleList(String title, String body) {
+        if(title != null && body == null) {
+            if(articleRepository.existsByTitle(title) == false) {
+                System.out.println("제목과 일치하는 게시물이 없습니다.");
+                return null;
+            }
+            return articleRepository.findByTitle(title);
+
+        } else if(title == null && body != null) {
+            if(articleRepository.existsByBody(body) == false) {
+                System.out.println("내용과 일치하는 게시물이 없습니다.");
+                return null;
+            }
+            return articleRepository.findByBody(body);
+
+        } else if(title != null && body != null) {
+            if(articleRepository.existsByTitleAndBody(title, body) == false) {
+                System.out.println("제목, 내용과 일치하는 게시물이 없습니다.");
+                return null;
+            }
+            return articleRepository.findByTitleAndBody(title, body);
+        }
+
         return articleRepository.findAll();
     }
 
