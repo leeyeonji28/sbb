e7774e55a377032690110b39a102e7bae7d178d0
leeyeonji28 <yeon102800@gmail.com>
2022-07-20 11:22:19 +0900

    필수항목 미입력 시 오류메시지 출력

--- src/main/java/com/mysite/sbb/Answer/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/Answer/controller/AnswerController.java
@@ -1,15 +1,18 @@
 package com.mysite.sbb.Answer.controller;
 
+import com.mysite.sbb.Answer.AnswerForm;
 import com.mysite.sbb.Answer.answer.AnswerService;
 import com.mysite.sbb.Question.domain.Question;
 import com.mysite.sbb.Question.service.QuestionService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/answer")
@@ -20,10 +23,16 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content){
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult){
         Question question = this.questionService.getQuestion(id);
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+
         // 질문 만들기
-        this.answerService.create(question, content);
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id); // 모든 작업이 끝나면 이 주소로 돌아오겠다는 뜻
     }
 
